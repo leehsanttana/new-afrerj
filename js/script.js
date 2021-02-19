2f0336105e5c24d2240635d173e60e9ae7a4d891
leehsanttana <leonamsanttana@gmail.com>
2021-02-19 09:41:54 -0300

    criando a área de login e o botao de whatsapp

--- js/script.js
+++ js/script.js
@@ -22,3 +22,36 @@ liParcerias.forEach((li, i) =>{
         activeTab(i);
     })
 })
+
+function modalLogin(){
+    const btnLogin = document.querySelector('[data-login="btn"]');
+    const modalLogin = document.querySelector('[data-login="modal"]');
+    const btnClose = document.querySelector('[data-login="close"]');
+
+    function openModalLogin(){
+        modalLogin.classList.add('active');
+    }
+
+    function closeModalLogin(event){
+        event.preventDefault();
+        modalLogin.classList.remove('active');
+    }
+
+    function cliqueForaModal(event){
+        if(event.target === this)
+        closeModalLogin(event);
+    }
+
+    btnLogin.addEventListener('click', openModalLogin);
+    btnClose.addEventListener('click', closeModalLogin);
+    modalLogin.addEventListener('click', cliqueForaModal);
+}
+
+modalLogin();
+
+function whatsappPopup(){
+    const btnWhatsapp = document.querySelector('.whatsapp');
+    btnWhatsapp.classList.add('open');
+}
+
+setTimeout(whatsappPopup, 2000);
