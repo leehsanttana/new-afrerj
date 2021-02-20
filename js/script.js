876e5f1d3c70cd0b9a66ee1ca366150f978e9f49
leehsanttana <leonamsanttana@gmail.com>
2021-02-20 14:33:13 -0300

    adicionando o menu principal do site

--- js/script.js
+++ js/script.js
@@ -49,6 +49,30 @@ function modalLogin(){
 
 modalLogin();
 
+function modalMenu(){
+    const btnMenu = document.querySelector('[data-menu="btn"]');
+    const modalMenu = document.querySelector('[data-menu="modal"]');
+
+    function openModalMenu(){
+        modalMenu.classList.toggle('active');
+    }
+
+    function closeModalMenu(event){
+        event.preventDefault();
+        modalMenu.classList.remove('active');
+    }
+
+    function cliqueForaModal(event){
+        if(event.target === this)
+        closeModalMenu(event);
+    }
+
+    btnMenu.addEventListener('click', openModalMenu)
+    modalMenu.addEventListener('click', cliqueForaModal);
+}
+
+modalMenu();
+
 function whatsappPopup(){
     const btnWhatsapp = document.querySelector('.whatsapp');
     btnWhatsapp.classList.add('open');
